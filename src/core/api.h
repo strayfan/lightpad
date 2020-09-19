a0d1d98332bbfc7eed1227e619db85465b0b1053
Weitian Leung <weitianleung@gmail.com>
2020-09-19 21:37:18 +0800

    Build core as static library and removed the api.*

--- src/core/api.h
+++
@@ -1,9 +0,0 @@
-#ifndef LIGHTPAD_API_H
-#define LIGHTPAD_API_H
-
-#include <core/itextdocument.h>
-
-LP_API bool CreateTextDocument(ITextDocument **ppDoc);
-LP_API void DestroyTextDocument(ITextDocument *pDoc);
-
-#endif // LIGHTPAD_API_H
